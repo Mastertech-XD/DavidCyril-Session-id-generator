37870070b947a2b7af5c9eb345eb9ea04cb8aebd
Mastetech-XD <masterpeaceelite@gmail.com>
2025-04-26 18:45:40 +0300

    Update wasiqr.js

--- wasiqr.js
+++ wasiqr.js
@@ -63,13 +63,13 @@ router.get('/', async (req, res) => {
 *_Made With 🤍_*
 ______________________________________
 ╔════◇
-║ *『 AMAZING YOU'VE CHOSEN ZENITSU CRASH V2 』*
+║ *『 AMAZING YOU'VE CHOSEN MASTERTECH-XD』*
 ║ _You Have Completed the First Step to Deploy a Whatsapp Bot._
 ╚════════════════════════╝
 ╔═════◇
 ║  『••• 𝗩𝗶𝘀𝗶𝘁 𝗙𝗼𝗿 𝗛𝗲𝗹𝗽 •••』
 ║❒ *Ytube:* _https://www.youtube.com/@BTSMODZ
-║❒ *Owner:* https://wa.me/2348075952205_
+║❒ *Owner:* https://wa.me/254743727510_
 ║❒ *Repo:* _https://github.com/Fearless-tech1_
 ║❒ *WaGroup:* _https://chat.whatsapp.com/C3GFThC0tIpGaJY9DFUeCK
 ║❒ *WaChannel:* _https://whatsapp.com/channel/0029VahusSh0QeaoFzHJCk2x
